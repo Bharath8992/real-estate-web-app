6863294b51b6d38e72e07b405772a807ce91584f
BHARATH <bharath122020@gmail.com>
2026-05-01 14:43:14 +0530

    updated image

--- appsource/mck_admin_console/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class mckAdminConsoleConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mck_admin_console'
--- appsource/mck_auth/templatetags/app_filter.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def update_underscore_with_space(value):
-    return value.replace("_"," ")
